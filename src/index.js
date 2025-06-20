c7d1df060b265b8fa45118d4866fa94eaedd5a65
jezzrs <jezz.ramirezs@gmail.com>
2025-06-20 20:08:38 +0100

    Update axios
    updating axios so it calls the API

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 const requestPoem = document.getElementById("submit-button");
 const poemElement = document.getElementById("poem");
+const instructionsInput = document.querySelector("instructions");
+const context =
+  "You are an expert in poems and your mission is to generate a 4 lines poem in basic HTML. Make sure you follow User instructions";
+const prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
+const apiKey = "b21d503dafo2f9t8344f5f54960e518f";
+const apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
 function generatePoem(event) {
   event.preventDefault();
+  axios.get(apiUrl).then(displayPoem);
+  console.log(`prompt: ${prompt}`);
+  console.log(`context: ${context}`);
+  console.log("Generating your poem...");
+}
 
-  new Typewriter("#poem", {
-    strings: "The Naming of Cats",
-    autoStart: true,
-    cursor: "",
-    delay: 50,
-  });
+function displayPoem(response) {
+  new Typewriter(poemElement),
+    {
+      strings: response.data.answer,
+      autoStart: true,
+      cursor: "",
+      delay: 1,
+    };
 }
 
 requestPoem.addEventListener("click", generatePoem);
-
-poemElement.inhher("The Naming of Cats");
