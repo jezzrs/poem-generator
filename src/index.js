a759278eda560d2d3fa5712d01221b092b972371
jezzrs <jezz.ramirezs@gmail.com>
2025-06-20 20:16:25 +0100

    updated java
    the code was faulty because it was recognising the ID

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const requestPoem = document.getElementById("submit-button");
 const poemElement = document.getElementById("poem");
-const instructionsInput = document.querySelector("instructions");
+const instructionsInput = document.querySelector("#instructions");
 const context =
   "You are an expert in poems and your mission is to generate a 4 lines poem in basic HTML. Make sure you follow User instructions";
+
 const prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
 const apiKey = "b21d503dafo2f9t8344f5f54960e518f";
 const apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
@@ -10,18 +11,16 @@ const apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=
 function generatePoem(event) {
   event.preventDefault();
   axios.get(apiUrl).then(displayPoem);
-  console.log(`prompt: ${prompt}`);
-  console.log(`context: ${context}`);
   console.log("Generating your poem...");
 }
 
 function displayPoem(response) {
   new Typewriter(poemElement),
     {
-      strings: response.data.answer,
+      strings: [response.data.answer],
       autoStart: true,
       cursor: "",
-      delay: 1,
+      delay: 75,
     };
 }
 
