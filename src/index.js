2696318d67c3219d9a785e44c8d4e402b81f82d3
jezzrs <jezz.ramirezs@gmail.com>
2025-06-21 20:43:33 +0100

    updating JS
    It wasnt pulling the poem from the AI properly. Changed some structured

--- src/index.js
+++ src/index.js
@@ -1,27 +1,38 @@
-const requestPoem = document.getElementById("submit-button");
-const poemElement = document.getElementById("poem");
-const instructionsInput = document.querySelector("#instructions");
-const context =
-  "You are an expert in poems and your mission is to generate a 4 lines poem in basic HTML. Make sure you follow User instructions";
+function generatePoem() {
+  const instructionsInput = document
+    .getElementById("instructions")
+    .value.trim();
+  if (!instructionsInput.trim()) {
+    alert("Please type a topic for your poem!");
+  }
+  const prompt = `Generate a poem about ${instructionsInput}`;
+  const context =
+    "You are an expert in poems and your mission is to generate a 4 lines poem in basic HTML. Make sure you follow User instructions. Do not add ``html before or after the poem, please.";
+  const apiKey = "b21d503dafo2f9t8344f5f54960e518f";
+  const apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-const prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
-const apiKey = "b21d503dafo2f9t8344f5f54960e518f";
-const apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-
-function generatePoem(event) {
-  event.preventDefault();
-  axios.get(apiUrl).then(displayPoem);
   console.log("Generating your poem...");
+  console.log(prompt);
+
+  axios.get(apiUrl).then(displayPoem);
+}
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  generatePoem();
 }
 
 function displayPoem(response) {
-  new Typewriter(poemElement),
-    {
-      strings: [response.data.answer],
-      autoStart: true,
-      cursor: "",
-      delay: 75,
-    };
+  console.log(response);
+  const poemElement = document.getElementById("poem");
+
+  new Typewriter(poemElement, {
+    strings: [response.data.answer],
+    cursor: "",
+    delay: 50,
+    loop: false,
+  }).start();
 }
 
-requestPoem.addEventListener("click", generatePoem);
+const requestPoem = document.getElementById("submit-button");
+requestPoem.addEventListener("click", handleSearchSubmit);
