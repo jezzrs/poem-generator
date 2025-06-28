ca658a6733c5543a1988a715bc04d74d2d2c12cf
jezzrs <jezz.ramirezs@gmail.com>
2025-06-28 19:29:59 +0100

    Updated HTML and JS
    The HTML url for axios wasnt correct.
    Cleaned JS, added the comments because it was not showing the poem properly.

--- src/index.js
+++ src/index.js
@@ -1,38 +1,53 @@
-function generatePoem() {
+//Event listener setup
+const requestPoem = document.getElementById("submit-button");
+requestPoem.addEventListener("click", generatePoem);
+
+//Generate poem (calling API)
+
+function generatePoem(event) {
+  event.preventDefault();
+  console.log("Generating your poem...");
+
+  //Get input or ask for input
   const instructionsInput = document
     .getElementById("instructions")
     .value.trim();
-  if (!instructionsInput.trim()) {
-    alert("Please type a topic for your poem!");
+  if (!instructionsInput) {
+    alert("Please enter a topic for your poem");
+    return;
   }
+
+  //Defining prompt and context
   const prompt = `Generate a poem about ${instructionsInput}`;
   const context =
     "You are an expert in poems and your mission is to generate a 4 lines poem in basic HTML. Make sure you follow User instructions. Do not add ``html before or after the poem, please.";
+
+  //Build API url
   const apiKey = "b21d503dafo2f9t8344f5f54960e518f";
-  const apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  const apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${encodeURIComponent(
+    prompt
+  )}&context=${encodeURIComponent(context)}&key=${apiKey}`;
 
-  console.log("Generating your poem...");
-  console.log(prompt);
-
+  //Call API
   axios.get(apiUrl).then(displayPoem);
 }
 
-function handleSearchSubmit(event) {
-  event.preventDefault();
-  generatePoem();
-}
-
+//Handle API response
 function displayPoem(response) {
-  console.log(response);
-  const poemElement = document.getElementById("poem");
+  console.log("API response", response);
 
+  const poemElement = document.getElementById("poem");
+  console.log("Poem is:", response.data);
+  const answer = response?.data?.answer || "(No poem returned)";
+
+  poemElement.innerHTML = `${JSON.stringify(answer, null, 2)}`;
+
+  //Typewrite animation
   new Typewriter(poemElement, {
-    strings: [response.data.answer],
+    strings: [answer],
+    autoStart: true,
     cursor: "",
     delay: 50,
     loop: false,
-  }).start();
+  });
 }
-
-const requestPoem = document.getElementById("submit-button");
-requestPoem.addEventListener("click", handleSearchSubmit);
