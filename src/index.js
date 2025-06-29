b2d74f884d09f8727e1224210bd48965c6a23ab2
jezzrs <jezz.ramirezs@gmail.com>
2025-06-29 11:01:34 +0100

    Updated CSS HTML JS
    changed the style of the input when the user writes for clarity
    changed the structure for the "loading" message to be centred properly
    Added a sand clock

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-//Event listener setup
+//Event listener when clicking
 const requestPoem = document.getElementById("submit-button");
 requestPoem.addEventListener("click", generatePoem);
 
-//Generate poem (calling API)
-
+//Generate poem = get instructions + defining prompt and context + set up API + call API
 function generatePoem(event) {
   event.preventDefault();
   console.log("Generating your poem...");
 
-  //Get input or ask for input
+  //Get user's instructions
   const instructionsInput = document
     .getElementById("instructions")
     .value.trim();
+
   if (!instructionsInput) {
     alert("Please enter a topic for your poem");
     return;
@@ -22,11 +22,18 @@ function generatePoem(event) {
   const context =
     "You are an expert in poems and your mission is to generate a 4 lines poem in basic HTML. Make sure you follow User instructions. Do not add ``html before or after the poem, please.";
 
-  //Build API url
+  //Set up API url
   const apiKey = "b21d503dafo2f9t8344f5f54960e518f";
   const apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${encodeURIComponent(
     prompt
   )}&context=${encodeURIComponent(context)}&key=${apiKey}`;
+
+  const poemElement = document.getElementById("poem");
+
+  poemElement.innerHTML = `${JSON.stringify([instructionsInput], null)}`;
+  poemElement.classList.remove("poem-hidden");
+  poemElement.classList.add("poem");
+  poemElement.innerHTML = `<div class=loading> <img src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/169/243/original/Sand_clock.gif?1751191140" alt="sand clock"> </br> We are generating your poem about ${instructionsInput}</div>`;
 
   //Call API
   axios.get(apiUrl).then(displayPoem);
@@ -34,19 +41,17 @@ function generatePoem(event) {
 
 //Handle API response
 function displayPoem(response) {
-  const poemElement = document.getElementById("poem");
   console.log("Poem generated");
   console.log("Poem is:", response.data);
 
-  const answer = response?.data?.answer || "(No poem returned)";
-  const poemWithCredit = `${answer}\n\n <div class=credits> — by She Codes AI</div>`;
+  const userInput = document.getElementById("instructions");
+  userInput.value = "";
 
-  poemElement.innerHTML = `${JSON.stringify(answer, null)}`;
-  poemElement.classList.remove("poem-hidden");
-  poemElement.classList.add("poem");
+  const answer = response?.data?.answer || "(No poem returned)";
+  const poemWithCredit = `${answer}\n\n <div class=credits> — by SheCodes AI</div>`;
 
   //Typewrite animation
-  new Typewriter(poemElement, {
+  new Typewriter("#poem", {
     strings: `${poemWithCredit}`,
     autoStart: true,
     cursor: "",
